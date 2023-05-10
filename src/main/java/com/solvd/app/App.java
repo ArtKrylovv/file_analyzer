21a13148ca9160284619c2b4d0c4db227ea9f6e4
ArtKrylov <akrlov91@gmail.com>
2023-05-09 20:13:38 -0700

    logger, try-cath, extended functionality

--- src/main/java/com/solvd/app/App.java
+++ src/main/java/com/solvd/app/App.java
@@ -8,27 +8,58 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+
 public class App {
-    public static void main(String[] args) throws IOException {
-        wordCounter1("src/main/resources/sample.txt");
+    private final static Logger LOGGER = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+
+        wordCounter2("src/main/resources/sample.txt", "Word count:");
     }
+
     // using for each loop to remove punctuation
-    public static void wordCounter1(String path) throws  IOException{
-        File file = new File(path);
-        String text = StringUtils.lowerCase(FileUtils.readFileToString(file, "UTF-8"));
-        String[] toRemove = {",", "?", "!", ".", "-", ":", ";", "/"};
-        for (String el : toRemove) {
-            text = StringUtils.remove(text, el);
+    public static void wordCounter1(String path, String myWord) {
+        try {
+            App.LOGGER.info("Reading file");
+            File file = new File(path);
+            String text = StringUtils.lowerCase(FileUtils.readFileToString(file, "UTF-8"));
+            String[] toRemove = {",", "?", "!", ".", "-", ":", ";", "/"};
+            for (String el : toRemove) {
+                text = StringUtils.remove(text, el);
+            }
+            Set<String> uniqueSet = new HashSet<>(Arrays.asList(StringUtils.split(text, " ")));
+            App.LOGGER.info("Creating new file at src/main/resources");
+            File newFile = new File ("src/main/resources/output.txt");
+            FileUtils.touch(newFile);
+            App.LOGGER.info("Writing to new file");
+            FileUtils.write(newFile, myWord+" "+uniqueSet.size(), "UTF-8", false);
+        } catch (IOException e) {
+            App.LOGGER.error(e.getMessage(), e);
         }
-        Set<String> uniqueSet = new HashSet<>(Arrays.asList(StringUtils.split(text, " ")));
-        FileUtils.write(file, "\n" + Integer.toString(uniqueSet.size()), "UTF-8", true);
     }
+
     // using for regex to remove punctuation
-    public static void wordCounter2(String path) throws IOException{
-        File file = new File(path);
-        String text = StringUtils.lowerCase(FileUtils.readFileToString(file, "UTF-8"));
-        text = text.replaceAll("[\\p{Punct}&&[^']]+", "");
-        Set<String> uniqueSet = new HashSet<>(Arrays.asList(StringUtils.split(text, " ")));
-        FileUtils.write(file, "\n" + Integer.toString(uniqueSet.size()), "UTF-8", true);
+    public static void wordCounter2(String path, String myWord) {
+        try {
+            App.LOGGER.info("Reading file");
+            File file = new File(path);
+            String text = StringUtils.lowerCase(FileUtils.readFileToString(file, "UTF-8"));
+            String[] toRemove = {",", "?", "!", ".", "-", ":", ";", "/"};
+            text = text.replaceAll("[\\p{Punct}&&[^']]+", "");
+            Set<String> uniqueSet = new HashSet<>(Arrays.asList(StringUtils.split(text, " ")));
+            App.LOGGER.info("Creating new file at src/main/resources");
+            File newFile = new File ("src/main/resources/output.txt");
+            FileUtils.touch(newFile);
+            App.LOGGER.info("Writing to new file");
+            FileUtils.write(newFile, myWord+" "+uniqueSet.size(), "UTF-8", false);
+        } catch (IOException e) {
+            App.LOGGER.error(e.getMessage(), e);
+        }
     }
 }
+
+
